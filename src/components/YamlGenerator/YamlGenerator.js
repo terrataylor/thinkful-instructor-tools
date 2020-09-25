5bf20c02ebb411ad4cdd7b9a4dee0d4454f363fa
Terra Taylor <terra.taylor@gmail.com>
2020-09-25 13:48:24 -0600

    yaml forms

--- src/components/YamlGenerator/YamlGenerator.js
+++ src/components/YamlGenerator/YamlGenerator.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Row, Col,Container, Button, Form, TextArea, FormGroup, Label, Input } from 'reactstrap';
 import apiUrl from '../../env';
 class YamlGenerator extends React.Component {
     state = {
@@ -53,11 +53,51 @@ class YamlGenerator extends React.Component {
                 <h2>YamlGenerator</h2>
                 <Form onSubmit={this.generateYaml}>
                     <FormGroup>
-                        <Label for="first">First Name</Label>
-                        <Input type="text" name="fname" id="first" onChange={this.onChange} value={this.state.fname === null ? '' : this.state.fname} />
+                        <Label for="courseCode">Course Code</Label>
+                        <select id="courseCode" class="form-control">
+                            <option value="dev-301">dev-301</option>
+                            <option value="data-301">data-301</option>
+                            <option value="data_analytics-301">data_analytics-301</option>
+                            <option value="digital_marketing-301">digital_marketing-301</option>
+                            <option value="project_marketing-301">project_marketing-301</option>
+                            <option value="ux-301">ux-301</option>
+                        </select>
                     </FormGroup>
                     <FormGroup>
+                        <Label for="cohortNum">Cohort Number</Label>
+                        <Input type="text" name="cohortNum" id="cohortNum" onChange={this.onChange} value={this.state.cohortNum === null ? '' : this.state.cohortNum} />
                     </FormGroup>
+                    <FormGroup>
+                        <Label for="startDate">Start Date</Label>
+                        <Input type="date" name="startDate" id="startDate" onChange={this.onChange} value={this.state.startDate === null ? '' : this.state.stateDate} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="workshopUrl">Workshop URL</Label>
+                        <Input type="url" name="workshopUrl" id="workshopUrl" onChange={this.onChange} value={this.state.workshopUrl === null ? '' : this.state.workshopUrl} />
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="timezone">Time Zone</Label>
+                        <select id="timezone" class="form-control">
+                            <option value="PST">PST</option>
+                            <option value="MST">MST</option>
+                            <option value="CST">CST</option>
+                            <option value="EST">EST</option>
+                        </select>
+                        </FormGroup>
+                        <FormGroup>
+                        <Label for="breakWeeks">BreakWeeks</Label>
+                        <Row>
+                            <Col><Input type="text" name="cohortNum" id="cohortNum" onChange={this.onChange} value={this.state.cohortNum === null ? '' : this.state.cohortNum} />
+                        </Col>
+                            <Col><Input type="text" name="cohortNum" id="cohortNum" onChange={this.onChange} value={this.state.cohortNum === null ? '' : this.state.cohortNum} /></Col>
+                        </Row>
+                    </FormGroup>
+                    
+                    <FormGroup>
+                        <Label for="studentList">Student List</Label>
+                        <textarea class="form-control" type="text" name="studentList" id="studentList" onChange={this.onChange} value={this.state.cohortNum === null ? '' : this.state.cohortNum} />
+                    </FormGroup>
+                    
                     <Button>Submit</Button>
                 </Form>
 
