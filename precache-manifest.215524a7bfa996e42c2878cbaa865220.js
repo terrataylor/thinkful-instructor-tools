43fe3f9cd4b82938fe1302a70d3a985cbdccc899
Terra <terra.taylor@gmail.com>
2020-10-01 11:05:16 -0600

    Updates

---
+++ precache-manifest.215524a7bfa996e42c2878cbaa865220.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d6b40a10b2eeb876a280d435834df5",
+    "url": "/thinkful-instructor-tools/index.html"
+  },
+  {
+    "revision": "66353647390986f38788",
+    "url": "/thinkful-instructor-tools/static/css/2.8186207c.chunk.css"
+  },
+  {
+    "revision": "f1f244f9e3568638f7db",
+    "url": "/thinkful-instructor-tools/static/css/main.2b5ba7ed.chunk.css"
+  },
+  {
+    "revision": "66353647390986f38788",
+    "url": "/thinkful-instructor-tools/static/js/2.7ac3abb4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/thinkful-instructor-tools/static/js/2.7ac3abb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f244f9e3568638f7db",
+    "url": "/thinkful-instructor-tools/static/js/main.e1a0af65.chunk.js"
+  },
+  {
+    "revision": "791359d46ec6e22fc8ec",
+    "url": "/thinkful-instructor-tools/static/js/runtime-main.84a842fe.js"
+  },
+  {
+    "revision": "e21a7e928185c20963690cd8de0ab0b7",
+    "url": "/thinkful-instructor-tools/static/media/view_a_ruberic(admins).e21a7e92.mp4"
+  }
+]);
